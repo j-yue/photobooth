aef232ff43d2068661b3838ce0b7edac32d76934
Jeanette Yue <yue.jeanette@gmail.com>
2021-10-31 01:27:06 -0700

    Remove portrait backgrounds and add more landscape backgrounds

--- src/BackgroundSelect.js
+++ src/BackgroundSelect.js
@@ -1,17 +1,41 @@
 import { Layer, Box, Avatar, Text, Heading } from "grommet";
 import background from "./images/background.jpeg";
-import abandoned from "./images/abandoned.jpeg";
 import ghost from "./images/ghost.jpeg";
-import tree from "./images/tree.jpeg";
 import charmed from "./images/charmed.jpg";
+import bats from "./images/bats.png";
+import batsghosts from "./images/batsghosts.png";
+import clouds from "./images/clouds.jpg";
+import heartsblack from "./images/heartsblackbg.png";
+import heartsblue from "./images/heartsbluebg.png";
+import hk from "./images/hk.png";
+import maruchan from "./images/maruchan.png";
+import neondreamscape from "./images/neondreamscape.png";
+import neonlights from "./images/neonlights.png";
+import neonwalls from "./images/neonwalls.png";
+import pastelhauntedhouse from "./images/pastelhauntedhouse.png";
+import pumpkin from "./images/pumpkin.png";
+import simpsons from "./images/simpsons.png";
+import space from "./images/space.png";
 
 const BackgroundSelect = ({ handleBgSelect }) => {
   const gallery = [
     [background, "scary mansion"],
-    [abandoned, "abandoned hallway"],
     [ghost, "ghost"],
-    [tree, "spooky tree"],
     [charmed, "the charmed house"],
+    [bats, "bats"],
+    [batsghosts, "bats, ghosts, and pumpkins"],
+    [clouds, "pastel clouds"],
+    [heartsblack, "hearts with black background"],
+    [heartsblue, "hearts with blue background"],
+    [hk, "hong kong"],
+    [maruchan, "maruchan noodles"],
+    [neondreamscape, "neondreamscape"],
+    [neonlights, "neon lights"],
+    [neonwalls, "neon walls"],
+    [pastelhauntedhouse, "pastel haunted house"],
+    [pumpkin, "pumpkin"],
+    [simpsons, "simpsons couch"],
+    [space, "space"],
   ];
   return (
     <Layer
